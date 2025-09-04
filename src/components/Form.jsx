b8944f61f68e04e88f8db606c6e0863b0433fee4
19nfyZ76 <paltatyana1@gmail.com>
2025-09-04 20:21:26 +0200

    add useEffect and

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,41 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
-const Form = ({onWeatherLoaded, onCityChange}) => {
+const Form = ({onWeatherLoaded}) => {
     const [city, setCity] = useState("");
     // eslint-disable-next-line no-undef
     const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    const REFRESH_INTERVAL =120000;
+
+    const fetchWeather = () => {
+        if(!city) return;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.cod === "404") {
+                    alert("City not found! Please check the spelling.");
+                    return;
+                }
+                onWeatherLoaded(data);
+            })
+            .catch(err => console.error("ERROR:", err));
+
+    };
 
     const handleSearch = () => {
         if (!city) {
             alert("Please enter a city name!");
             return;
         }
-        onCityChange(city);
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-            .then(res => res.json())
-            .then(data => {
-                onWeatherLoaded(data);
-            })
-            .catch(err => console.error("ERROR:", err));
+        fetchWeather();
     };
+
+    useEffect(() =>{
+        if (!city) return;
+        const interval = setInterval(() =>{
+            fetchWeather()
+        }, REFRESH_INTERVAL);
+        return () => clearInterval(interval);
+        },[city]);
 
     return (
         <div>
